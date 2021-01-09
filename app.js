d7321e8b3c901908adad3a3a76c11a9d472adc83
Harry Bolingot <harrybolingot@gmail.com>
2021-01-09 15:09:03 +0800

    Finished game (with game over checker)

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const playerScore2 = document.getElementById('score-1');
 const playerCurrentScore2 = document.getElementById('current-1');
 
 var diceMin = 1, diceMax = 6, roundNumber = 0;
+var isGameOver;
+
 class Player {
     currentScore = 0;
     totalScore = 0;
@@ -49,48 +51,81 @@ function startNewGame(){
     playerScore2.innerHTML = 0;
     playerCurrentScore1.innerHTML = 0;
     playerCurrentScore2.innerHTML = 0;
+    document.getElementById("gameState").style.display = "none";
 }
 
 function rollDice(){
-    // get random number between 1 and 6
-    let diceRollValue = randomNumber(diceMin, diceMax);
-    console.log(diceRollValue);
-    document.getElementById('dice').src = "dice-" + diceRollValue + ".png";
-    if(player1.turn === true) {
-        if(diceRollValue === 1) {
-            player1.currentScore = 0;
-            changeTurn();
+    if(isGameOver === true) {
+        startNewGame();
+    }
+
+    else {
+        // get random number between 1 and 6
+        let diceRollValue = randomNumber(diceMin, diceMax);
+        console.log(diceRollValue);
+        document.getElementById('dice').src = "dice-" + diceRollValue + ".png";
+        if(player1.turn === true) {
+            if(diceRollValue === 1) {
+                player1.currentScore = 0;
+                changeTurn();
+            } else {
+                player1.updateScore(diceRollValue);
+            }
+            console.log('PLAYER SCORE: ' + player1.currentScore);
+            playerScore1.innerHTML = player1.currentScore;
         } else {
-            player1.updateScore(diceRollValue);
-        }
-        console.log('PLAYER SCORE: ' + player1.currentScore);
-        playerScore1.innerHTML = player1.currentScore;
-    } else {
-        if(diceRollValue === 1) {
-            player2.currentScore = 0;
-            changeTurn();
-        } else {
-            player2.updateScore(diceRollValue);
-        }
-        console.log('PLAYER SCORE: ' + player2.currentScore);
-        playerScore2.innerHTML = player2.currentScore;
-    } 
+            if(diceRollValue === 1) {
+                player2.currentScore = 0;
+                changeTurn();
+            } else {
+                player2.updateScore(diceRollValue);
+            }
+            console.log('PLAYER SCORE: ' + player2.currentScore);
+            playerScore2.innerHTML = player2.currentScore;
+        } 
+    }
     // console.log("You rolled " + diceRollValue + "!\n" + "Player name: " + player.name + "\nCurrent score: " + player.currentScore + "\nStill your turn?: " + player.turn);
 }
 
 function holdScore() {
-    if(player1.turn === true) {
-        player1.playerHold();
-        player1.currentScore = 0;
-        console.log('PLAYER HELD: ' + player1.totalScore)
-        playerCurrentScore1.innerHTML = player1.totalScore;
-    } else {
-        player2.playerHold();
-        player2.currentScore = 0;
-        console.log('PLAYER HELD: ' + player2.totalScore)
-        playerCurrentScore2.innerHTML = player2.totalScore;
+    if(isGameOver === true) {
+        startNewGame();
     }
-    changeTurn();
+
+    else {
+        if(player1.turn === true) {
+            player1.playerHold();
+            player1.currentScore = 0;
+            console.log('PLAYER HELD: ' + player1.totalScore)
+            playerCurrentScore1.innerHTML = player1.totalScore;
+        } else {
+            player2.playerHold();
+            player2.currentScore = 0;
+            console.log('PLAYER HELD: ' + player2.totalScore)
+            playerCurrentScore2.innerHTML = player2.totalScore;
+        }
+    
+        if (player1.totalScore >= 100){
+            document.getElementById("player-1-panel").classList.add("active");
+            document.getElementById("player-1-panel").classList.add("winner");
+            document.getElementById("gameState").style.display = "block";
+            isGameOver = true;
+        }
+    
+        else if (player2.totalScore >= 100){
+            document.getElementById("player-2-panel").classList.add("winner");
+            document.getElementById("player-2-panel").classList.add("winner");
+            document.getElementById("gameState").style.display = "block";
+            isGameOver = true;
+        }
+    
+        else {
+            isGameOver = false;
+            changeTurn();
+        }
+    }
+
+    
 }
 function changeTurn() {
     player1.turn = !player1.turn;
