ae59c04ee87f4b615ba280afdfa19767bc0dbb1a
aflamiano-std <aflamiano.learning@gmail.com>
2021-01-09 11:31:46 +0800

    Added addPlayerScore() functionality

--- app.js
+++ app.js
@@ -1,10 +1,39 @@
-/*
-GAME RULES:
+// functions to build program:
 
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
+// roll dice()
+// check dice()
+// reset if 1 ()
+// check roll score()
+// add roll score()
+// hold()
+// add score to total()
+// check player()
 
-*/
+const playerName1 = document.getElementById('name-0');
+const playerScore1 = document.getElementById('score-0');
+const playerCurrentScore1 = document.getElementById('current-0');
+const playerName2 = document.getElementById('name-1');
+const playerScore2 = document.getElementById('score-1');
+const playerCurrentScore2 = document.getElementById('current-1');
+let totalScore;
+
+addPlayerScore(player, turn) {
+    let currentTurn = turn % 2;
+    if(currentTurn === 0) {
+        //add diceRoll score to playerScore
+        playerName1.innerHTML = player.Name;
+        playerScore1.innerHTML = player.score;
+        playerCurrentScore1.innerHTML = player.currentScore;
+    } else {
+        playerName2.innerHTML = player.Name;
+        playerScore2.innerHTML = player.score;
+        playerCurrentScore2.innerHTML = player.currentScore;
+    }
+}
+
+holdScore(player) {
+    //add playerScore to totalScore
+    //display totalScore
+}
+
+
